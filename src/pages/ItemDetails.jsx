665de3c6cc384ef6be0846eb226a935144342c16
TEST User <test@example.com>
2026-08-05 18:03:24 -0400

    newItems dynamic, carousel, timer, and loading state added

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,13 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import EthImage from "../images/ethereum.svg";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
-import nftImage from "../images/nftImage.jpg";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios'
+import Skeleton from '../components/UI/Skeleton'
 
 const ItemDetails = () => {
+  const { id } = useParams()
+  const [items, setItems] = useState([])
+  const [loading, setLoading] = useState(true)
+  const item = items.find(item => item.nftId == id)
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    async function fetchData() {
+      const [collectionsD, newItemsD] = await Promise.all([
+        axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections`),
+        axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems`)
+      ])
+      const combined = [...collectionsD.data, ...newItemsD.data]
+      setItems(combined)
+      setLoading(false)
+    }
+    fetchData()
+  }, [id]);
+
 
   return (
     <div id="wrapper">
@@ -16,73 +32,128 @@ const ItemDetails = () => {
         <section aria-label="section" className="mt90 sm-mt-0">
           <div className="container">
             <div className="row">
-              <div className="col-md-6 text-center">
-                <img
-                  src={nftImage}
-                  className="img-fluid img-rounded mb-sm-30 nft-image"
-                  alt=""
-                />
-              </div>
-              <div className="col-md-6">
-                <div className="item_info">
-                  <h2>Rainbow Style #194</h2>
-
-                  <div className="item_info_counts">
-                    <div className="item_info_views">
-                      <i className="fa fa-eye"></i>
-                      100
-                    </div>
-                    <div className="item_info_like">
-                      <i className="fa fa-heart"></i>
-                      74
-                    </div>
+              {loading ? (
+                <>
+                  <div className="col-md-6 text-center">
+                    <Skeleton width="100%" height="100%" />
                   </div>
-                  <p>
-                    doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-                    illo inventore veritatis et quasi architecto beatae vitae
-                    dicta sunt explicabo.
-                  </p>
-                  <div className="d-flex flex-row">
-                    <div className="mr40">
-                      <h6>Owner</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                  <div className="col-md-6">
+                    <div className="item_info">
+                      <Skeleton width="100%" height="46px" />
+                      <div className="item_info_counts">
+                        <div className="item_info_views">
+                          <Skeleton width="100%" height="100%" />
                         </div>
-                        <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                        <div className="item_info_like">
+                          <Skeleton width="100%" height="100%" />
+                        </div>
+                      </div>
+                      <Skeleton width="100%" height="78px" />
+                      <div className="d-flex flex-row">
+                        <div className="mr40">
+                          <h6>Owner</h6>
+                          <div className="item_author">
+                            <div className="author_list_pp">
+                                <Skeleton width="50px" height="50px" borderRadius="50%" />
+                            </div>
+                            <div className="author_list_info">
+                              <Skeleton width="100px" height="20px" />
+                            </div>
+                          </div>
+                        </div>
+                        <div></div>
+                      </div>
+                      <div className="de_tab tab_simple">
+                        <div className="de_tab_content">
+                          <h6>Creator</h6>
+                          <div className="item_author">
+                            <div className="author_list_pp">
+                             <Skeleton width="50px" height="50px" borderRadius="50%" />
+                            </div>
+                            <div className="author_list_info">
+                              <Skeleton width="100px" height="20px" />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="spacer-40"></div>
+                        <h6>Price</h6>
+                        <div className="nft-item-price">
+                          <Skeleton width="100px" height="30px" />
                         </div>
                       </div>
                     </div>
-                    <div></div>
                   </div>
-                  <div className="de_tab tab_simple">
-                    <div className="de_tab_content">
-                      <h6>Creator</h6>
-                      <div className="item_author">
-                        <div className="author_list_pp">
-                          <Link to="/author">
-                            <img className="lazy" src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                          </Link>
+                </>
+              ) : (
+                <>
+                  <div className="col-md-6 text-center">
+                    <img
+                      src={item?.nftImage}
+                      className="img-fluid img-rounded mb-sm-30 nft-image"
+                      alt=""
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <div className="item_info">
+                      <h2>{item?.title} #194</h2>
+                      <div className="item_info_counts">
+                        <div className="item_info_views">
+                          <i className="fa fa-eye"></i>
+                          100
                         </div>
-                        <div className="author_list_info">
-                          <Link to="/author">Monica Lucas</Link>
+                        <div className="item_info_like">
+                          <i className="fa fa-heart"></i>
+                          {item.likes}
+                        </div>
+                      </div>
+                      <p>
+                        doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+                        illo inventore veritatis et quasi architecto beatae vitae
+                        dicta sunt explicabo.
+                      </p>
+                      <div className="d-flex flex-row">
+                        <div className="mr40">
+                          <h6>Owner</h6>
+                          <div className="item_author">
+                            <div className="author_list_pp">
+                              <Link to="/author">
+                                <img className="lazy" src={item?.authorImage} alt="" />
+                                <i className="fa fa-check"></i>
+                              </Link>
+                            </div>
+                            <div className="author_list_info">
+                              <Link to="/author">Monica Lucas</Link>
+                            </div>
+                          </div>
+                        </div>
+                        <div></div>
+                      </div>
+                      <div className="de_tab tab_simple">
+                        <div className="de_tab_content">
+                          <h6>Creator</h6>
+                          <div className="item_author">
+                            <div className="author_list_pp">
+                              <Link to="/author">
+                                <img className="lazy" src={item?.authorImage} alt="" />
+                                <i className="fa fa-check"></i>
+                              </Link>
+                            </div>
+                            <div className="author_list_info">
+                              <Link to="/author">Monica Lucas</Link>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="spacer-40"></div>
+                        <h6>Price</h6>
+                        <div className="nft-item-price">
+                          <img src={EthImage} alt="" />
+                          <span>{item.price}</span>
                         </div>
                       </div>
                     </div>
-                    <div className="spacer-40"></div>
-                    <h6>Price</h6>
-                    <div className="nft-item-price">
-                      <img src={EthImage} alt="" />
-                      <span>1.85</span>
-                    </div>
                   </div>
-                </div>
-              </div>
+                </>
+              )}
             </div>
           </div>
         </section>
